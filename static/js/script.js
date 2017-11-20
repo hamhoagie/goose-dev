aaf567556978368cb6c9951d1a26d7ce0ff37b58
billy mcdermott <billy@billymcdermott.com>
2017-11-20 12:39:31 -0500

    this is the last time i try to implement

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,16 @@
 element.addEventListener("touchstart", touchStart, false);
 element.addEventListener("touchend", touchEnd, false);
 
+function touchstart(event) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+};
+
+function touchsend(event) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+};
 
 
 }).call(this);
