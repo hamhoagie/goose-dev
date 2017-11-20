1e1722073f4b3b1fc9025c60170ffed0a226c4db
billy mcdermott <billy@billymcdermott.com>
2017-11-20 12:21:08 -0500

    events events events

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,4 @@
 (function() {
-(function ($) {
-    $.fn.tclick = function (onclick) {
-        this.bind("touchstart", function (e) { onmousedown.call(this, e); e.stopPropagation(); e.preventDefault(); });
-        his.bind("touchend", function (e) { onmouseup.call(this, e); e. stopPropogation(); e.preventDefault(); });
-        this.bind("click", function (e) { onclick.call(this, e); });
-        return this;
-    };
-})
 
 }).call(this);
 
